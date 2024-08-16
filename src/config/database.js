bcf1b43279951d89a6d7b2324fba532d9cb00aec
Adenilson365 <adekonzelmann@gmail.com>
2024-08-15 21:11:42 -0300

    Corrigindo devContainer, adicionando  Migrations

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+
+ module.exports = {
+    dialect: 'postgres',
+    host: '172.27.10.232',
+    username: 'admin',
+    password: 'admin',
+    database: 'tcc',
+    define: {
+      timestamps: true, 
+      underscored: true,
+      underscoredAll: true, 
+    }
+}; 
+
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from "express"
+import User from "./app/models/User";
+import { password } from "./config/database";
 
 const routes = new Router();
 
-routes.get("/", (req, res)=>{
-    return res.json({ok: true})
+routes.get("/teste", async (req, res)=>{
+    const user = await User.create({
+        name: 'Adenilson2',
+        email: "teste@teste2",
+        password_hash: '123123'
+    })
+    
+    return res.json(user)
 })
 
 
